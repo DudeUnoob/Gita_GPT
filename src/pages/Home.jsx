24e31318a333edf3ce0890efaea1fae96705eaf4
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-02-13 20:14:45 -0600

    frontend changes and responsive design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import { callApi } from '../functions/api';
 import "../public/Home.css"
 
 
+
 export default function Home() {
     const darkTheme = useTheme()
     const toggleTheme = useThemeUpdate()
@@ -90,25 +91,80 @@ export default function Home() {
                 <h2 style={{ display: "inline-block" }}>Example Questions</h2>
                 <br />
                 <br />
-                <Container className="sampleQuestions" >
+                <Container className='sampleQuestions'>
                     <Row>
-                        {["What is the purpose of life?", "What is 9.26?", "What is Karma Yoga?", "What is the soul?"].map((elm, i) => {
+                        {[ "What's the purpose of life?","What is 9.26?", "What is Karma Yoga?", "What is the soul?"].map((elm, i) => {
                             return (
 
-                                <Col key={i} lg="auto">
-                                    <Card onClick={() => inputText.current.value = elm} bg={darkTheme == true ? "dark" : "light"} >
+                                <Col  key={i} sm>
+                                    <Card  style={{height:"10rem", marginBottom:"0.5rem"}}  onClick={() => inputText.current.value = elm} bg={darkTheme == true ? "dark" : "light"} className="card">
                                         <Card.Body>
                                             <Card.Title style={{color: darkTheme == true ? "white" : "black"}}>{elm}</Card.Title>
                                         </Card.Body>
                                     </Card>
-                                </Col>
+                                    </Col>
 
                             )
 
                         })}
                     </Row>
-                </Container>
+                    </Container>
+
+               
             </div>
+            {/* <footer>
+  <div className="container">
+    <div className="row">
+      <div className="col-md-4">
+        <h4>About Us</h4>
+        <p>Free Open Gita AI 😀📔 available to anyone!</p>
+        
+      </div>
+      <div className="col-md-4">
+        <h4>Contact Me</h4>
+        <p>Get in touch with me for any questions or concerns.</p>
+        <a id="fa" href="https://www.linkedin.com/in/damodar-kamani-a7204123a/" target="_blank"><i className="fa fa-linkedin-square" style={{fontSize:"30px"}}></i></a>
+        <a id="fa" href="https://github.com/dudeunoob" target="_blank"><i className="fa fa-github" style={{fontSize:"30px"}}></i></a>
+      </div>
+      <div className="col-md-4">
+        <h4>Follow Us</h4>
+        <p>Stay updated with our latest news and events.</p>
+      </div>
+    </div>
+    <div className="row">
+      <p className="copyright">Copyright © 2023 All rights reserved.</p>
+    </div>
+  </div>
+</footer> */}
+
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <h5>About Me/This</h5>
+        <p>In February of 2023 I decided to make a website </p>
+        <p>dedicated to a free open Gita AI 📔</p>
+        <p><b>Created by: <a href="https://github.com/DudeUnoob" target={"_blank"}>@dom✌</a></b></p>
+      </div>
+      <div class="col-md-4">
+        <h5>Check Me Out!</h5>
+        <p><i class="fa fa-linkedin" ></i> <a href="https://www.linkedin.com/in/damodar-kamani-a7204123a/" target={"_blank"}>Linkedin</a></p>
+        <p><i class="fa fa-github" style={{paddingRight:"5px"}}></i><a href="https://github.com/DudeUnoob" target={"_blank"}>Github</a></p>
+        <p><i class="fa fa-envelope"></i> techdomprogramming@gmail.com</p>
+      </div>
+      <div class="col-md-4">
+        <h5>Follow Me</h5>
+        {/* <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a> */}
+        <a href="https://twitter.com/DudeUnoob69" target={"_blank"}><i class="fa fa-twitter-square fa-2x"></i></a>
+        <a href="https://www.instagram.com/damodar_kamanii/" target={"_blank"}><i class="fa fa-instagram fa-2x"></i></a>
+      </div>
+    </div>
+    <hr />
+    <p class="text-center">Copyright © 2023 Gita GPT</p>
+  </div>
+</footer>
+
+            
         </>
     )
 }
