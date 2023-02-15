06adbf3a752f7c1c6b95b3efced531e4f2835d27
Damodar Kamani <44341783+DudeUnoob@users.noreply.github.com>
2023-02-14 19:53:08 -0600

    new chat history

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,112 +7,114 @@ import "../public/Home.css"
 
 
 export default function Home() {
-    const darkTheme = useTheme()
-    const toggleTheme = useThemeUpdate()
+  const darkTheme = useTheme()
+  const toggleTheme = useThemeUpdate()
 
-    const inputText = useRef(null)
-    const [messageValue, setMessageValue] = useState(null)
-    const [loading, setLoading] = useState(false)
+  const inputText = useRef(null)
+  const [messageValue, setMessageValue] = useState(null)
+  const [loading, setLoading] = useState(false)
 
-    const handleCallGPT = async () => {
-        const chatHistory = window.localStorage.getItem("chat_history")
-        const message = inputText.current.value
-        //[`Human: ${message}\nAI: ${data?.answer}`]
-        if (chatHistory) {
-            setMessageValue(null)
-            setLoading(true)
-            const data = await callApi(message, JSON.parse(chatHistory))
+  const handleCallGPT = async () => {
+    const chatHistory = window.localStorage.getItem("chat_history")
+    const chatScrollHistory = window.localStorage.getItem("scrolling_chat_history")
+    const message = inputText.current.value
+    //[`Human: ${message}\nAI: ${data?.answer}`]
+    if (chatHistory) {
+      setMessageValue(null)
+      setLoading(true)
+      const data = await callApi(message, JSON.parse(chatHistory))
 
-            setLoading(false)
-            setMessageValue(data)
-            const getStorage = JSON.parse(chatHistory)
-            const messageArr = [`Human: ${message}\nAI: ${data?.answer}`]
-
-            const finalArr = getStorage.concat(messageArr)
-
-            window.localStorage.setItem("chat_history", JSON.stringify(finalArr))
-
-
-
-        } else {
-            setMessageValue(null)
-            setLoading(true)
-            const data = await callApi(message)
-            setLoading(false)
-            setMessageValue(data)
-            window.localStorage.setItem("chat_history", JSON.stringify([`Human: ${message}\nAI: ${data?.answer}`]))
+      setLoading(false)
+      setMessageValue(data)
+      const getStorage = JSON.parse(chatHistory)
+      const messageArr = [`Human: ${message}\nAI: ${data?.answer}`]
+      const testScrollingObj = {
+        chat: {
+          Human: message,
+          AI: data?.answer
         }
+      }
+      const scrollingChatObj = [
+        testScrollingObj
+      ]
+
+      const finalScrollingChatArr = JSON.parse(chatScrollHistory).concat(scrollingChatObj)
+      const finalArr = getStorage.concat(messageArr)
+
+      window.localStorage.setItem("scrolling_chat_history", JSON.stringify(finalScrollingChatArr))
+      window.localStorage.setItem("chat_history", JSON.stringify(finalArr))
 
 
+
+    } else {
+      setMessageValue(null)
+      setLoading(true)
+      const data = await callApi(message)
+      setLoading(false)
+      setMessageValue(data)
+      const testScrollingObj = {
+        chat: {
+          Human: message,
+          AI: data?.answer
+        }
+      }
+      window.localStorage.setItem("chat_history", JSON.stringify([`Human: ${message}\nAI: ${data?.answer}`]))
+      window.localStorage.setItem("scrolling_chat_history", JSON.stringify(
+        [testScrollingObj]
+      ))
     }
 
-    const loadingStyleState = { visibility: loading == true ? "visible" : "hidden" }
-    const secondLoadingStyleState = { display: loading == true ? "inline-block" : "none" }
 
-    return (
-        <>
+  }
 
-            <div className="heading">
-                <h1>Welcome to Gita GPT! 😀</h1>
-                <p>Ask any question related to the bhagavad gita! ex: Verses, Krishna, Arjuna, etc...</p>
-
-            </div>
-            <br />
-            <br />
-            <div className="card_body">
+  const getStorage = JSON.parse(window.localStorage.getItem("scrolling_chat_history"))
 
 
-                <InputGroup className="mb-3" placeholder='Ask a question!' id="input_question">
+  const loadingStyleState = { visibility: loading == true ? "visible" : "hidden" }
+  const secondLoadingStyleState = { display: loading == true ? "inline-block" : "none" }
 
-                    <Form.Control
-                        aria-label="Example text with button addon"
-                        aria-describedby="basic-addon1"
-                        placeholder='Ask any question related to the bhagavad gita!'
-                        ref={inputText}
-                        required
-                        onKeyDown={(e) => e.code == "Enter" ? handleCallGPT() : ""}
-                    />
-                    <Button id="button-addon1" onClick={handleCallGPT} disabled={loading} variant={darkTheme == true ? "dark" : "primary"}>
-                        Send
-                    </Button>
+  return (
+    <>
 
-                </InputGroup>
+      <div className="heading">
+        <h1>Welcome to Gita GPT! 😀</h1>
+        <p>Ask any question related to the bhagavad gita! ex: Verses, Krishna, Arjuna, etc...</p>
 
-                <div className="answers">
-                    <div className="loadingState">
-                        <Spinner animation='border' role={"status"} style={secondLoadingStyleState}>
-                            <span className='visually-hidden'>Loading...</span>
-                        </Spinner>
-                    </div>
-                    <p id="answer_result"><b>{messageValue?.answer}</b></p>
-                </div>
-                <br />
-                <br />
-                <h2 style={{ display: "inline-block" }}>Example Questions</h2>
-                <br />
-                <br />
-                <Container className='sampleQuestions'>
-                    <Row>
-                        {[ "What's the purpose of life?","What is 9.26?", "What is Karma Yoga?", "What is the soul?"].map((elm, i) => {
-                            return (
+      </div>
+      <br />
+      <br />
+      <div className="card_body">
 
-                                <Col  key={i} sm>
-                                    <Card  style={{height:"10rem", marginBottom:"0.5rem"}}  onClick={() => inputText.current.value = elm} bg={darkTheme == true ? "dark" : "light"} className="card">
-                                        <Card.Body>
-                                            <Card.Title style={{color: darkTheme == true ? "white" : "black"}}>{elm}</Card.Title>
-                                        </Card.Body>
-                                    </Card>
-                                    </Col>
 
-                            )
+        <InputGroup className="mb-3" placeholder='Ask a question!' id="input_question">
 
-                        })}
-                    </Row>
-                    </Container>
+          <Form.Control
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+            placeholder='Ask any question related to the bhagavad gita!'
+            ref={inputText}
+            required
+            onKeyDown={(e) => e.code == "Enter" ? handleCallGPT() : ""}
+          />
+          <Button id="button-addon1" onClick={handleCallGPT} disabled={loading} variant={darkTheme == true ? "dark" : "primary"}>
+            Send
+          </Button>
 
-               
-            </div>
-            {/* <footer>
+        </InputGroup>
+
+        <div className="answers">
+          <div className="loadingState">
+            <Spinner animation='border' role={"status"} style={secondLoadingStyleState}>
+              <span className='visually-hidden'>Loading...</span>
+            </Spinner>
+          </div>
+          <p id="answer_result"><b>{messageValue?.answer}</b></p>
+        </div>
+        
+
+
+      </div>
+      {/* <footer>
   <div className="container">
     <div className="row">
       <div className="col-md-4">
@@ -136,35 +138,74 @@ export default function Home() {
     </div>
   </div>
 </footer> */}
+      <div className="containerScroll">
+        <div className="scrollable">
+          {getStorage.map((elm, i) => {
+            return (
+              <div key={i} className="chat-container">
+                <div className='chat-message human_chat' style={{ float: "right" }}>
+                  <p className='message'>{elm.chat.Human}</p>
+                </div>
+                <div className="chat-message ai_chat" style={{ float: "left" }}>
+                  <p className='message'>{elm.chat.AI}</p>
+                </div>
 
-<footer>
-  <div className="container">
-    <div className="row">
-      <div className="col-md-4">
-        <h5>About Me/This</h5>
-        <p>In February of 2023 I decided to make a website </p>
-        <p>dedicated to a free open Gita AI 📔</p>
-        <p><b>Created by: <a href="https://github.com/DudeUnoob" target={"_blank"}>@dom✌</a></b></p>
+              </div>
+            )
+          })}
+        </div>
       </div>
-      <div className="col-md-4">
-        <h5>Check Me Out!</h5>
-        <p><i className="fa fa-linkedin" ></i> <a href="https://www.linkedin.com/in/damodar-kamani-a7204123a/" target={"_blank"}>Linkedin</a></p>
-        <p><i className="fa fa-github" style={{paddingRight:"5px"}}></i><a href="https://github.com/DudeUnoob" target={"_blank"}>Github</a></p>
-        <p><i className="fa fa-envelope"></i> techdomprogramming@gmail.com</p>
-      </div>
-      <div className="col-md-4">
-        <h5>Follow Me</h5>
-        {/* <a href="#"><i className="fa fa-facebook-square fa-2x"></i></a> */}
-        <a href="https://twitter.com/DudeUnoob69" target={"_blank"}><i className="fa fa-twitter-square fa-2x"></i></a>
-        <a href="https://www.instagram.com/damodar_kamanii/" target={"_blank"}><i className="fa fa-instagram fa-2x"></i></a>
-      </div>
-    </div>
-    <hr />
-    <p className="text-center">Copyright © 2023 Gita GPT</p>
-  </div>
-</footer>
+      <br />
+      <h2 style={{  textAlign:"center" }}>Example Questions</h2>
+        <br />
+        <br />
+        <Container className='sampleQuestions'>
+          <Row>
+            {["What's the purpose of life?", "What is 9.26?", "What is Karma Yoga?", "What is the soul?"].map((elm, i) => {
+              return (
 
-            
-        </>
-    )
+                <Col key={i} sm>
+                  <Card style={{ height: "10rem", marginBottom: "0.5rem" }} onClick={() => inputText.current.value = elm} bg={darkTheme == true ? "dark" : "light"} className="card">
+                    <Card.Body>
+                      <Card.Title style={{ color: darkTheme == true ? "white" : "black" }}>{elm}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Col>
+
+              )
+
+            })}
+          </Row>
+        </Container>
+
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <h5>About Me/This</h5>
+              <p>In February of 2023 I decided to make a website </p>
+              <p>dedicated to a free open Gita AI 📔</p>
+              <p><b>Created by: <a href="https://github.com/DudeUnoob" target={"_blank"}>@dom✌</a></b></p>
+            </div>
+            <div className="col-md-4">
+              <h5>Check Me Out!</h5>
+              <p><i className="fa fa-linkedin" ></i> <a href="https://www.linkedin.com/in/damodar-kamani-a7204123a/" target={"_blank"}>Linkedin</a></p>
+              <p><i className="fa fa-github" style={{ paddingRight: "5px" }}></i><a href="https://github.com/DudeUnoob" target={"_blank"}>Github</a></p>
+              <p><i className="fa fa-envelope"></i> techdomprogramming@gmail.com</p>
+            </div>
+            <div className="col-md-4">
+              <h5>Follow Me</h5>
+              {/* <a href="#"><i className="fa fa-facebook-square fa-2x"></i></a> */}
+              <a href="https://twitter.com/DudeUnoob69" target={"_blank"}><i className="fa fa-twitter-square fa-2x"></i></a>
+              <a href="https://www.instagram.com/damodar_kamanii/" target={"_blank"}><i className="fa fa-instagram fa-2x"></i></a>
+            </div>
+          </div>
+          <hr />
+          <p className="text-center">Copyright © 2023 Gita GPT</p>
+        </div>
+      </footer>
+
+
+    </>
+  )
 }
